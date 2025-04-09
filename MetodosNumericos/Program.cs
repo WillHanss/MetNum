ad80abc2f1a72860c661a3b49c62ffe6d3401d94
WillHanss <williamhanssensanchez@gmail.com>
2025-04-09 14:06:20 -0600

    Code Refractoring
    Code refractoring

--- MetodosNumericos/Program.cs
+++ MetodosNumericos/Program.cs
@@ -1,1 +1,31 @@
-﻿System.Console.WriteLine("Hola");
+﻿System.Console.WriteLine("Trapezoidal Rule");
+
+//Ask the user for the lower limit (a), upper limit (b), and number of intervals (n)
+System.Console.WriteLine("Enter the lower limit (a): "); 
+double a = Convert.ToDouble(System.Console.ReadLine()); // Upper limit
+
+System.Console.WriteLine("Enter the upper limit (b): ");
+double b = Convert.ToDouble(System.Console.ReadLine()); // Lower limit
+
+System.Console.WriteLine("Enter the number of intervals (n): ");
+int n = Convert.ToInt32(System.Console.ReadLine()); // Number of intervals
+
+//Calculate the width of each interval
+static double TrapezoidalRule(double a, double b, int n)
+{
+    double h = (b - a) / n;
+    double sum = func(a) + func(b);
+
+    for (int i = 1; i < n; i++)
+    {
+        double x = a + i * n;
+        sum += 2 * func(x);
+    }
+
+    return (h / 2) * sum;
+}
+
+static double func(double x)
+{
+    return Math.Pow(x, 3) + 10 * Math.Pow(x, 2) + 8 * x + 10;
+}
